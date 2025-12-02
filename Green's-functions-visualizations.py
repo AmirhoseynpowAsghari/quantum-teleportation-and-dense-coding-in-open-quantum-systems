90d44e0a660c3e0aaac6c2255ccc1497597ef688
Amir hoseyn <amirhoseynasghari.pow@gmail.com>
2025-12-02 16:20:10 +0330

    self-consistent

---
+++ Green's-functions-visualizations.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from self_consistent_equations import solve_self_consistent
+from greens_function import compute_greens_functions
+
+u_ks, v_ks, Delta_ks, E_ks, mu, KX, KY, epsilon_ks = solve_self_consistent(
+    Nk=200,
+    t=0.1,
+    U=45*0.1,
+    Delta_t=0.45,
+    V_SO=0.0,
+    n=1.875
+)
+
+kx = np.linspace(-np.pi, np.pi, 200)
+ky = np.linspace(-np.pi, np.pi, 200)
+
+# -----------------------
+# Step 2: compute G(r,θ), F(r,θ)
+# -----------------------
+r_vals, theta_vals, G_polar, F_polar = compute_greens_functions(
+    u_ks, v_ks, KX, KY, kx, ky,
+    r_max=10,
+    Nr=200
+)
+
+
+# -----------------------
+# Step 3: plot results
+# -----------------------
+
+plt.figure(figsize=(8, 6))
+for i, theta in enumerate(theta_vals):
+    plt.plot(r_vals, (G_polar[i, :]).imag, label=f"θ = {np.degrees(theta):.0f}°")
+plt.xlabel("r")
+plt.ylabel("$G_{uu}(r, θ)$")
+plt.legend()
+plt.grid(True)
+plt.title("Normal Green's Function in Polar Coordinates")
+plt.show()
+
+plt.figure(figsize=(8, 6))
+for i, theta in enumerate(theta_vals):
+    plt.plot(r_vals, (F_polar[i, :]).imag, label=f"θ = {np.degrees(theta):.0f}°")
+plt.xlabel("r")
+plt.ylabel("$F_{ud}(r, θ)$")
+plt.legend()
+plt.grid(True)
+plt.title("Anomalous Green's Function in Polar Coordinates")
+plt.show()
+
